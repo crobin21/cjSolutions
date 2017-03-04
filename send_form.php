87b36374ed4b8b5ff1a3d311111efe4bf08ca77e
Cesar Robinson <cesar_robinson@cable.comcast.com>
2017-03-04 17:15:39 -0500

    Latest website changes

--- send_form.php
+++ send_form.php
@@ -1,13 +1,11 @@
 <?php
-    if(isset($_POST['submit'])){
     $to = "contact@cj-itsolutions.com";
     $from = $_POST['email'];
     $name = $_POST['name'];
-    $subject = "Form Submission";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['comment'];
+    $subject = "Form submission from: " . $name;
+    $message = "The customer wrote the following:" . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
     mail($to,$subject,$message,$headers);
-    echo "Your comment has been sent. Thank you " . $name . "!";
-    }
+    header("Location: /#contact")
 ?>
