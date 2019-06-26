cce1c13a046c9974a21a97e8d22a7bb7075b1232
Cesar Robinson <cesar_robinson@comcast.com>
2019-06-26 09:38:13 -0400

    Prevent spammy emails to flood my inbox

--- CjSolutions/es/envia_mensaje.php
+++ CjSolutions/es/envia_mensaje.php
@@ -6,6 +6,8 @@
     $message = "The customer wrote the following:" . "\n\n" . $_POST['message'];
 
     $headers = "From:" . $from;
-    mail($to,$subject,$message,$headers);
+    if(!preg_match("/^\d.\w*/",$message)) {
+        mail($to,$subject,$message,$headers);
+    }
     header("Location: /es/#contact")
 ?>
